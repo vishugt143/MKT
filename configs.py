11ac7b318c33a05f4339bc210e86d5bd4ccb297a
vishugt143 <158712030+vishugt143@users.noreply.github.com>
2026-02-25 11:54:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,12 +3,12 @@ from os import getenv
 class Config:
     API_ID = int(getenv("API_ID", "21419016"))
     API_HASH = getenv("API_HASH", "79198e1eb4cfd0f771a89d83b9144e7e")
-    BOT_TOKEN = getenv("BOT_TOKEN", "8231212589:AAHMoF6lruAcu6b7Xa-C9ComuZSEo6RGJPE")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8333213303:AAHzkaE2nwR4VN3JlQZ1xwcxqmNE5aPazk4")
 
     # Admin / Owner IDs (space-separated)
     SUDO = list(map(int, getenv(
         "SUDO",
-        "7554081592 5306138654 7564050858 5656436152"
+        "7554081592 6355771377 7564050858 5656436152"
     ).split()))
 
     MONGO_URI = getenv(
@@ -18,9 +18,8 @@ class Config:
 
     # Posts to copy
     POSTS = [
-        "https://t.me/forward_hack_lnx/166",
-        "https://t.me/forward_hack_lnx/167",
-        "https://t.me/forward_hack_lnx/168"
+        "https://t.me/forward_hack_lnx/214",
+        "https://t.me/forward_hack_lnx/215"
     ]
 
     # 🚫 ILLEGAL WORDS (BOT SIDE FILTER)
